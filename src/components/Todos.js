5f092d690945da70a3d9f54ef9c02ff4453ea53f
paul <guinardpaul@gmail.com>
2017-09-13 19:43:42 +0200

    add todo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,34 +6,73 @@ export class Todos extends Component {
         super(props);
         this.state = {
             todos: [
-                {id: 1, titre: 'azee'},
-                {id: 2, titre: 'ezaa'},
-                {id: 3, titre: 'zzzzzz'}
-            ]
-        }
+                { id: 1, titre: 'Apprendre React' },
+                { id: 2, titre: 'Vivement Redux' },
+                { id: 3, titre: 'setState est relou' }
+            ],
+            todo: { id: 0, titre: '' }
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.addTodo = this.addTodo.bind(this);
+        this.getId=this.getId.bind(this);
     }
 
-    deleteTodo(todo){
+    addTodo(event) {
+        this.state.todo.id = this.getId()
+        console.log(this.state.todo);
         const newState = this.state.todos;
-        if(newState.indexOf(todo) > -1){
-            newState.splice(newState.indexOf(todo), 1)
+        newState.push(this.state.todo);
+        this.setState({
+            todos: newState
+        });
+        this.state.todo = {id: 0, titre: ''};
+        event.preventDefault();
+    }
+
+    getId() {
+        let id;
+        for (var item in this.state.todos) {
+            if (this.state.todos.hasOwnProperty(item)) {
+                id = this.state.todos[item].id
+            }
+        }
+        id++
+        return id;
+    }
+
+    deleteTodo(item) {
+        const newState = this.state.todos;
+        if (newState.indexOf(item) > -1) {
+            newState.splice(newState.indexOf(item), 1)
             this.setState({
                 todos: newState
             });
         }
     }
-    
+
+    handleChange(event) {
+        this.setState({
+            todo: {titre: event.target.value}
+        });
+    }
+
     render() {
         const listTodos = this.state.todos.map((todo, index) => {
-                        return <li key={index}>
-                            <span>{todo.id} {todo.titre}</span>
-                            <button onClick={this.deleteTodo.bind(this, todo)} className="btn btn-danger">Delete</button>
-                        </li>
+            return <li key={index} className="list-group-item">
+                {todo.id}{todo.titre}
+                <button onClick={this.deleteTodo.bind(this, todo)} className="btn btn-danger">Delete</button>
+            </li>
         });
 
-        return(
+        return (
             <div>
-                <ul>
+                <form className="form-inline" onSubmit={this.addTodo}>
+                    <div className="form-group">
+                        <input type="text" name="todo" id="todo" value={this.state.todo.titre} onChange={this.handleChange} className="form-control" placeholder="New Todo . . ." />
+                        <button type="submit" className="btn btn-primary">Créer</button>
+                    </div>
+                </form>
+                <ul className="list-group">
                     {listTodos}
                 </ul>
             </div>
@@ -41,7 +80,3 @@ export class Todos extends Component {
     }
 }
 
-Todos.PropTypes = {
-    todo: PropTypes.object,
-    todos: PropTypes.array
-}
